17913094408b248d94205d3fbea0db7a362aa14b
kanishkar-star <71880783+kanishkar-star@users.noreply.github.com>
2021-03-08 14:02:34 +0530

    Add files via upload

---
+++ bottles.js
@@ -0,0 +1,47 @@
+status = '';
+objects = [];
+
+function preload(){
+    first_img = loadImage('Bottle.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(680, 500);
+    canvas.center();
+    objDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = "Status = Detecting Objects";
+}
+
+function draw(){
+    image(first_img, 0, 0, 680, 500);
+
+    if(status != ''){
+        for(i = 0; i < objects.length; i++){
+            percent = floor(objects[i].confidence * 100);
+            object_name = objects[i].label;
+            fill('#FF0000');
+            text(object_name +' ' + percent + "%", objects[i].x, objects[i].y);
+            stroke('#FF0000');
+            noFill();
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log('Model Loaded');
+    status = true;
+    objDetector.detect(first_img, gotResult);
+}
+
+function gotResult(error, result){
+    if(error){
+        console.error(error);
+    }
+    console.log(result);
+    objects = result;
+}
+
+function back(){
+    window.location = 'home.html'
+}
---
+++ home.js
@@ -0,0 +1,19 @@
+function first(){
+    window.location = 'big_bottles.html'
+}
+
+function second(){
+    window.location = 'books.html'
+}
+
+function third(){
+    window.location = 'bottles.html'
+}
+
+function fourth(){
+    window.location = 'comp.html'
+}
+
+function do_something(){
+    window.location = 'pen.html';
+}
